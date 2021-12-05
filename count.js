7db1270a425904e03659e95a4890e28aa2286902
utkarsh639 <69382459+utkarsh639@users.noreply.github.com>
2021-12-05 10:27:37 +0530

    Add files via upload

---
+++ count.js
@@ -0,0 +1,12 @@
+var data = 0;
+ document.getElementById("change_no").innerText = data
+
+ function like(){
+     data += 1;
+     document.getElementById("change_no").innerText =data }
+
+ function dislike(){
+    data -= 1;
+    document.getElementById("change_no").innerText = data
+}
+
